1a0251550a231de698a3ef724436236c06b0e2db
RomanShmi <romyli4ka@gmail.com>
2021-09-20 08:09:37 -0400

    final edition

--- CarGarage/Car.cs
+++ CarGarage/Car.cs
@@ -12,5 +12,36 @@ namespace CarGarage
         public int FuelLevel { get; set; }
         public bool AutoBrakeActive { get; set; }
 
+
+        public void Accelerate() 
+        { 
+                this.Speed = 10; 
+           
+            if (this.Make == "Ferrari") this.Speed += 10; }
+        
+        
+        
+        public void SetModel(string mod) { this.Model = mod; }
+
+        public void SetMake(string mak)
+        { this.Make = mak; }
+       public int GetSpeed() { return this.Speed; }
+
+        public bool IsSpeeding()
+        {
+            bool spd = false;
+            if (this.Speed > 70) { spd = true; } else { spd = false; };
+            return spd;
+        }
+        public void Brake()
+        {
+            if (this.Speed ==1) this.Speed = 0;
+            if (this.Speed == 10) this.Speed = 3;
+
+            if (this.AutoBrakeActive) this.Speed = this.Speed / 2;
+        }
+        //public void Brake(bool d) { if (AutoBrakeActive) this.Speed = this.Speed / 2; }
+
+
     }
 }
